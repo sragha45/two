5851b86ecc940e5740dc23f87503986550eb47fa
Raghav <sragha45@gmail.com>
2018-02-14 13:50:33 +0530

    cleared all unwanted components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,14 @@ import { HttpModule } from '@angular/http';
 import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
+
+
 
 @NgModule({
     declarations: [
         AppComponent,
-        AboutComponent,
-        HomeComponent
+        
+
     ],
     imports: [
         BrowserAnimationsModule,
